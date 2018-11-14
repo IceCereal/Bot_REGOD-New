c300147c1a988d7728dea6f369f980c62fbb24cc
IceCereal <ns_raghav@hotmail.com>
2018-11-15 00:23:04 +0530

    Add SubredditLog + minor fixes

--- Modules/Log/Log.py
+++ Modules/Log/Log.py
@@ -9,6 +9,9 @@ class Log:
             writeMode_regular:  a:  append (default)
             fileName_error:     Name of the error log file:     Bot_REGOD-Error-Log (default)
             writeMode_error:    a:  append (default)
+
+            fileName_subreddit: Name of the Subreddit Logger:   Bot_REGOD-Subreddit-Log (immutable)
+            writeMode_subreddit:    a:  append (immutable)
         """
 
         if fileName_regular == None:
@@ -28,6 +31,9 @@ class Log:
         self.fileName_error = fileName_error
         self.writeMode_error = writeMode_error
 
+        self.fileName_subreddit = "Bot_REGOD-Subreddit-Log"
+        self.writeMode_subreddit = 'a'
+
 
     def errorLog(self, exception : str, number : int):
         """
@@ -40,7 +46,7 @@ class Log:
             ------------------
         """
 
-        fileObj = open(self.fileName_error, self.writeMode_error)
+        fileObj = open("Modules/Log/Logs/" + self.fileName_error, self.writeMode_error)
 
         fileObj.write("\n\n"+str(datetime.now()))
         fileObj.write("\nError Number:\t"+str(number))
@@ -57,8 +63,22 @@ class Log:
             [TIMESTAMP] | [Logging String]
         """
 
-        fileObj = open(self.fileName_regular, self.writeMode_regular)
+        fileObj = open("Modules/Log/Logs/" + self.fileName_regular, self.writeMode_regular)
 
         fileObj.write("\n[" + str(datetime.now()) + "] | [" + loggingString + "]")
+
+        fileObj.close()
+
+    def SubredditLog(self, subreddit : str):
+        """
+            LOGGING FUNCTION TO LOG ONLY SUBREDDIT'S
+
+            Writes it as:
+            |{[TIMESTAMP], [Subreddit]}
+        """
+
+        fileObj = open("Modules/Log/Logs/" + self.fileName_subreddit, self.writeMode_subreddit)
+
+        fileObj.write("|{[" + str(datetime.now()) + "], [" + subreddit + "]}")
 
         fileObj.close()
